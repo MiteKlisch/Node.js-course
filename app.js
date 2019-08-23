b02f3d2f6449706b097a156f35c53390b7aac2c5
Volodymyr <klischvolodimir@gmail.com>
2019-08-23 16:45:23 +0300

    sd

---
+++ app.js
@@ -0,0 +1,6 @@
+// const FirstName = require('./utils.js');
+
+// console.log(FirstName);
+const getNotes = require('./notes.js');
+massage = getNotes;
+console.log(massage);
---
+++ notes.js
@@ -0,0 +1,6 @@
+function getNotes() {
+    return 'Your Notes...';
+}
+
+console.log('note.js');
+module.exports = getNotes();
