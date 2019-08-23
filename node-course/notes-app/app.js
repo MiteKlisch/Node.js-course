e02189a9dc2002f8639d9779441d2edb8e801823
Volodymyr <klischvolodimir@gmail.com>
2019-08-24 00:52:35 +0300

    Completed 3rd part

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,6 +1,16 @@
 // const FirstName = require('./utils.js');
 
 // console.log(FirstName);
+
+const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
-massage = getNotes;
+
+const massage = getNotes;
 console.log(massage);
+
+//Example of using 'Validator'
+console.log(validator.isURL('https://www.youtube.com/?hl=uk&gl=UA'));
+//Example of using 'Chalk'
+console.log(chalk.green.inverse.bold('Success'));
+
