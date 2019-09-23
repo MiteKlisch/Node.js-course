f03b6b465a9f8a3e21eca7ff249761ad5a68e361
Volodymyr <klischvolodimir@gmail.com>
2019-09-24 00:31:39 +0300

    weather-app

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,9 +1,9 @@
 const request = require('request');
 
 
-const url = 'https://api.darksky.net/forecast/9e7798a23023b395aef09a22ed063e6d/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/9e7798a23023b395aef09a22ed063e6d/37.8267,-122.4233?lang=uk';
 request({url: url, json: true}, (error, response)=>{
   //console.dir(response.body.currently, {depth: null});
-  console.dir('It is currently '+ response.body.currently.temperature + ' degrees out. '+ 
+  console.dir(response.body.daily.data[0].summary + ' It is currently '+ response.body.currently.temperature + ' degrees out. '+ 
   'There is ' + response.body.currently.precipProbability + '% chance of rain', {depth:null});
  });
