4ac6c2c14aea29a0022e99224151ca8be007a04f
Volodymyr <klischvolodimir@gmail.com>
2019-10-20 00:59:20 +0300

    added some files to gitignore

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,7 +1,8 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
-
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
 
 
 const app = express();
@@ -46,10 +47,56 @@ app.get('/help', (req, res) => {
 
 //weather page
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is sunny',
-        location: 'Lviv'
+    if (!req.query.adress) {
+        res.send({
+            error: 'You must provide adress term'
+        })
+    }
+    geocode(req.query.adress, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({ error });
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error });
+            }
+           
+            res.send({
+                forecast: forecastData, 
+                location,
+                adress: req.query.adress
+            })
+        })
     })
+});
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide search term'
+        })
+    }
+
+    console.log(req.query.search);
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Roma Zalupa',
+        errorMessage: 'Help article not found'
+    });
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Roma Zalupa',
+        errorMessage: 'Page not found'
+    });
 })
 
 app.listen(3000, () => {
